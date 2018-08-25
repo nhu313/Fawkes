9f0eb1884fb9fe50ec50bde0a85f47881c37da44
Nhu Nguyen <nhu313@gmail.com>
2018-08-25 09:10:57 -0400

    added talk id to speaker

--- lib/fawkes/schedule/speaker.ex
+++ lib/fawkes/schedule/speaker.ex
@@ -20,7 +20,7 @@ defmodule Fawkes.Schedule.Speaker do
   @doc false
   def changeset(speaker, attrs) do
     speaker
-    |> cast(attrs, [:slug, :image_url, :first, :last, :company, :github, :twitter, :description])
+    |> cast(attrs, [:slug, :image_url, :first, :last, :company, :github, :twitter, :description, :talk_id])
     |> validate_required([:slug, :image_url, :first, :last])
     |> unique_constraint(:slug)
   end
