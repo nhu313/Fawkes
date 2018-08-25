7bb0deaee77ebe54b404fba26833c945ea7a7ff9
Nhu Nguyen <nhu313@gmail.com>
2018-08-25 09:06:01 -0400

    added slot

--- lib/fawkes_web/views/slot_view.ex
+++ lib/fawkes_web/views/slot_view.ex
@@ -1,3 +1,12 @@
 defmodule FawkesWeb.SlotView do
   use FawkesWeb, :view
+
+  def conference_dates do
+    format = "{YYYY}-{M}-{D}"
+
+    [Timex.parse!("2018-09-04", format),
+     Timex.parse!("2018-09-05", format),
+     Timex.parse!("2018-09-06", format),
+     Timex.parse!("2018-09-07", format)]
+  end
 end
